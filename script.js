f45eee0a598850df7b09ad6732a970dc41f953f6
Rodrigo <rodrigo.v.araujo.g@gmail.com>
2024-06-19 11:20:35 -0300

    remove eval function

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 const display = document.querySelector(".display")
 const button = document.querySelectorAll("button")
-const ac = document.querySelector(".ac")
-let = lastInputWasOperator = false
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let resultDisplayed = false;
+
+function calculate(a, b, operator) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch(operator) {
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return a / b;
+        default: return 0;
+    }
+}
 
 button.forEach(b => {
     b.addEventListener("click", function() {
         const value = b.textContent.trim();
-        let calc = ""
 
-        if(value === "AC" || value === "C") {
-            display.textContent = ""
-        }else if(value === "="){
-            display.textContent = eval(calc)
-        }else{
-            calc += value
-            display.textContent += calc
-        }
-
-        // se o display estiver vazio ele mostra AC, se houver algum numero mostre C 
-        if(display.textContent === ""){
-            ac.textContent = "AC"
-        }else{
-            ac.textContent = "C"
+        if(value == "AC" || value == "C"){
+            display.textContent = "";   
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+            resultDisplayed = false;
+        }else if(firstNumber && operator && display.textContent){
+            secondNumber = display.textContent;
+            display.textContent = calculate(firstNumber, secondNumber, operator);
+            firstNumber = display.textContent;
+            secondNumber = "";
+            operator = "";
+            resultDisplayed = true;
+        }else if(["+", "-", "*", "/"].includes(value)){
+            if(firstNumber && operator && display.textContent){
+                secondNumber = display.textContent;
+                display.textContent = calculate(firstNumber, secondNumber, operator);
+                firstNumber = display.textContent;
+                secondNumber = "";
+                operator = value;
+                resultDisplayed = false;
+            }else {
+                firstNumber = display.textContent;
+                operator = value;
+                display.textContent = "";
+            }
+        }else {
+            if(resultDisplayed){
+                display.textContent = "";
+                resultDisplayed = false;
+            }
+            display.textContent += value;
         }
     })
 })
-
-
